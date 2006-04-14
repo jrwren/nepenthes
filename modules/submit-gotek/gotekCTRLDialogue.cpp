75fc26b6ba2dc998243870e0b475c8a60570373f
common <common@carnivore.it>
2006-04-14 00:12:48 +0000

    nepenthes - submit-gotek, include #include <sys/types.h> for openbsd

--- modules/submit-gotek/gotekCTRLDialogue.cpp
+++ modules/submit-gotek/gotekCTRLDialogue.cpp
@@ -26,6 +26,9 @@
  *******************************************************************************/
 
  /* $Id$ */
+
+#include <sys/types.h>
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
--- modules/submit-gotek/gotekDATADialogue.cpp
+++ modules/submit-gotek/gotekDATADialogue.cpp
@@ -26,6 +26,9 @@
  *******************************************************************************/
 
  /* $Id$ */
+
+#include <stdint.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
