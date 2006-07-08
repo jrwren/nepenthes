5f51655a4ab075e81800e97b30a09a86797a039b
common <common@carnivore.it>
2006-07-08 15:25:45 +0000

    nepenthes  - log-download   - include types.h for openbsd

--- modules/log-download/log-download.cpp
+++ modules/log-download/log-download.cpp
@@ -26,8 +26,8 @@
  *******************************************************************************/
 
  /* $Id$ */
- 
-#include <stdint.h>
+
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
