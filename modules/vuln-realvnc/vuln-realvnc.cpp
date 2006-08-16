1adc307dc4df06d34bd8995da4a47552457f7d12
common <common@carnivore.it>
2006-08-16 17:47:51 +0000

    nepenthes  - vuln-realvnc, improved decoding, decreased spam

--- modules/vuln-realvnc/vuln-realvnc.cpp
+++ modules/vuln-realvnc/vuln-realvnc.cpp
@@ -204,14 +204,24 @@ RealVNCDialogue::~RealVNCDialogue()
 
 	logCrit("VNCCommandSession '%s'\n",m_TypedChars.c_str());	
 
-    if ( (offset = m_TypedChars.find("cmd")) < m_TypedChars.size() ||
+    if ( (offset = m_TypedChars.find("cmd")) < m_TypedChars.size()  ||
 		 (offset = m_TypedChars.find("echo")) < m_TypedChars.size() ||
-		 (offset = m_TypedChars.find("tftp")) < m_TypedChars.size() )
+		 (offset = m_TypedChars.find("tftp")) < m_TypedChars.size() 
+
+		 )
 	{
 		string command = m_TypedChars.substr(offset,m_TypedChars.size() - offset);
-		logCrit("offset %i '%s'\n",(int)offset,command.c_str());
+		logCrit("command offset %i '%s'\n",(int)offset,command.c_str());
 		
+	}else
+    if ( (offset = m_TypedChars.find("http://")) < m_TypedChars.size() ||
+			 (offset = m_TypedChars.find("ftp://")) < m_TypedChars.size() )
+	{
+			string command = m_TypedChars.substr(offset,m_TypedChars.size() - offset);
+			logCrit("download offset %i '%s'\n",(int)offset,command.c_str());
 	}
+	
+
 }
 
 /**
@@ -232,9 +242,9 @@ ConsumeLevel RealVNCDialogue::incomingData(Message *msg)
 
 	
 
-	switch (m_State)
+	if ( m_State == VNC_HANDSHAKE )
 	{
-	case VNC_HANDSHAKE:
+	
 		logSpam("VNC_HANDSHAKE\n");
 		g_Nepenthes->getUtilities()->hexdump((byte *)m_Buffer->getData(),m_Buffer->getSize());
 		if (m_Buffer->getSize() >= strlen(rfb_version_003_008) && 
@@ -247,10 +257,11 @@ ConsumeLevel RealVNCDialogue::incomingData(Message *msg)
 
 			m_State = VNC_AUTH;
 		}
-		break;
+	}
 
-	case VNC_AUTH:
-		logSpam("VNC_AUTH\n");
+	if ( m_State == VNC_AUTH)
+	{
+    	logSpam("VNC_AUTH\n");
 		g_Nepenthes->getUtilities()->hexdump((byte *)m_Buffer->getData(),m_Buffer->getSize());
 		if (m_Buffer->getSize() >= 1 )
 		{
@@ -264,10 +275,12 @@ ConsumeLevel RealVNCDialogue::incomingData(Message *msg)
 				m_State = VNC_SHARED_DESKTOP;
 			}
 		}
-		break;
+	}
+//		break;
 
-	case VNC_SHARED_DESKTOP:
-		logSpam("VNC_SHARED_DESKTOP\n");
+	if ( m_State == VNC_SHARED_DESKTOP)
+	{
+    	logSpam("VNC_SHARED_DESKTOP\n");
 		if ( m_Buffer->getSize() >= 1 )
 		{
 			m_Buffer->cut(1);
@@ -282,11 +295,12 @@ ConsumeLevel RealVNCDialogue::incomingData(Message *msg)
             m_State = VNC_ONLINE;
 
 		}
-		break;
+	}
+//		break;
 
-	case VNC_ONLINE:
-		
-		logSpam("VNC_ONLINE\n");
+	if ( m_State == VNC_ONLINE )
+	{
+    	logSpam("VNC_ONLINE\n");
 		{
 			bool needmore=false;
 
@@ -347,8 +361,8 @@ ConsumeLevel RealVNCDialogue::incomingData(Message *msg)
 					break;
 
 				case 4:
-					logSpam("ClientReq: KeyEvent\n");
-					g_Nepenthes->getUtilities()->hexdump((byte *)m_Buffer->getData(),m_Buffer->getSize());
+//					logSpam("ClientReq: KeyEvent\n");
+//					g_Nepenthes->getUtilities()->hexdump((byte *)m_Buffer->getData(),m_Buffer->getSize());
 					if (m_Buffer->getSize() >= 8 )
 					{
 						uint32_t skey;
@@ -360,16 +374,19 @@ ConsumeLevel RealVNCDialogue::incomingData(Message *msg)
 						byte *updown = ((byte *)m_Buffer->getData()+1);
 
 
-						printf("updown '%x'\n",(unsigned int)*updown);
+//						printf("updown '%x'\n",(unsigned int)*updown);
 						if ( *updown != 0 )
 						{
 							if ( key[2] == 0 )
 							{
 								if ( isalpha( key[3] )  || isspace( key[3] ) || isprint( key[3] ) )
 								{
-									printf("key %c\n",key[3]);
+									logSpam("key %c\n",key[3]);
 									m_TypedChars += key[3];
 									logSpam("Session is %s\n",m_TypedChars.c_str());
+								}else
+								{
+									logSpam("key %02x\n",key[3]);
 								}
 							}
 							else
@@ -379,8 +396,133 @@ ConsumeLevel RealVNCDialogue::incomingData(Message *msg)
 								{
 
 								case 0x0d:	// return 
+									logSpam("key [return]\n");
 									m_TypedChars += "\n";
 									break;
+
+								case 0x08: // BackSpace   
+									logSpam("key [backspace]\n");
+									break;
+
+								case 0x09: // Tab         
+									logSpam("key [tab]\n");
+									break;
+
+								case 0x1b: // Escape      
+									logSpam("key [esc]\n");
+									break;
+
+								case 0x63: // Insert 
+									logSpam("key [insert]\n");
+									break;
+
+								case 0xff: // Delete    
+									logSpam("key [delete]\n");
+									break;
+
+								case 0x50: // Home        
+									logSpam("key [home]\n");
+									break;
+
+								case 0x57: // End     
+									logSpam("key [end]\n");
+									break;
+
+								case 0x55: // Page Up
+									logSpam("key [page up]\n");
+									break;
+
+								case 0x56: // Page Down 
+									logSpam("key [page down]\n");
+									break;
+
+								case 0x51: // Left
+									logSpam("key [left]\n");
+									break;
+
+								case 0x52: // Up      
+									logSpam("key [up]\n");
+									break;
+
+								case 0x53: // Right   
+									logSpam("key [right]\n");
+									break;
+
+								case 0x54: // Down    
+									logSpam("key [down]\n");
+									break;
+
+								case 0xbe: // F1      
+									logSpam("key [f1]\n");
+									break;
+
+								case 0xbf: // F2      
+									logSpam("key [f2]\n");
+									break;
+
+								case 0xc0: // F3
+									logSpam("key [f3]\n");
+									break;
+
+								case 0xc1: // F4      
+									logSpam("key [f4]\n");
+									break;
+
+								case 0xc2: // F5
+									logSpam("key [f5]\n");
+									break;
+
+								case 0xc3: // F6      
+									logSpam("key [f6]\n");
+									break;
+
+								case 0xc4: // F7  
+									logSpam("key [f7]\n");
+									break;
+
+								case 0xc5: // F8    
+									logSpam("key [f8]\n");
+									break;
+
+								case 0xc6: // F9      
+									logSpam("key [f9]\n");
+									break;
+
+								case 0xc7: // F10     
+									logSpam("key [f10]\n");
+									break;
+
+								case 0xc8: // F11     
+									logSpam("key [f11]\n");
+									break;
+
+								case 0xc9: // F12     
+									logSpam("key [f12]\n");
+									break;
+
+								case 0xe1: // Shift   
+									logSpam("key [shift]\n");
+									break;
+
+								case 0xe3: // Control
+									logSpam("key [control]\n");
+									break;
+
+								case 0xe7: // Meta
+									logSpam("key [meta]\n");
+									break;
+
+								case 0xe9: // Alt    
+									logSpam("key [alt]\n");
+									break;
+
+								case 0xeb:	// Windows key
+									logSpam("key [windows]\n");
+									break;
+
+								default:
+									logSpam("key [unknown] code %02x\n",key[3]);
+
 								}
 							}
 						}
@@ -436,8 +578,7 @@ ConsumeLevel RealVNCDialogue::incomingData(Message *msg)
 				}
 			}
 		}
-		break;
-	}
+    }
 
 
 
