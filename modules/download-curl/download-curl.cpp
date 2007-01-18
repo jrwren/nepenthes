cdced533d13135e09ed5af72786363a384434c75
oxff <oxff@carnivore.it>
2007-01-18 23:22:24 +0000

    nepenthes.download-curl: adopted to new API, thanks to Philip Olsson for providing this patch

--- modules/download-curl/download-curl.cpp
+++ modules/download-curl/download-curl.cpp
@@ -230,7 +230,9 @@ bool CurlDownloadHandler::download(Download *down)
 				 down->getDownloadUrl()->getPort(),
 				 down->getDownloadUrl()->getPath().c_str());
 //        string auth = down->getDownloadUrl()->getUser() + ":" + down->getDownloadUrl()->getPass();
+#if LIBCURL_VERSION_NUM < 0x071000
 		curl_easy_setopt(pCurlHandle, CURLOPT_SOURCE_USERPWD,(char *)down->getDownloadUrl()->getAuth().c_str());
++#endif
 		curl_easy_setopt(pCurlHandle, CURLOPT_USERPWD,(char *)down->getDownloadUrl()->getAuth().c_str());
 		curl_easy_setopt(pCurlHandle, CURLOPT_URL			, url);
 		curl_easy_setopt(pCurlHandle, CURLOPT_FTP_RESPONSE_TIMEOUT, 120);	// 2 min ftp timeout
