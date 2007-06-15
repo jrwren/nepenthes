63a6f16321bccdaf8e30b1e5215317c5beca6d4a
oxff <oxff@carnivore.it>
2007-06-15 18:32:04 +0000

    fixed the ordering of includes, obsd sucks :)

--- modules/module-peiros/module-peiros.cpp
+++ modules/module-peiros/module-peiros.cpp
@@ -29,8 +29,8 @@
 
 #include <ctype.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
